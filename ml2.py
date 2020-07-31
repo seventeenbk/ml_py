965327ad49fa57f5563d7d6caae02795d1438188
seventeenbk <karthikb444@gmail.com>
2020-07-31 08:01:56 +0530

    Update ml2.py

--- ml2.py
+++ ml2.py
@@ -1,3 +1,20 @@
 import pandas as pd
 df = pd.read_csv("Bengaluru_House_data.csv")
 print(df.shape)
+#print(df.head())
+#print(df["area_type"].head())
+#print(df.area_type.head())
+#print(df["location"][6])
+
+#indexing elements, row first, column second
+#print(df.iloc[2])
+#print(df.iloc[:,4])
+#print(df.iloc[1:11,5])
+#print(df.iloc[1:10:2,5])
+#print(df.iloc[-1:-10:-2,5])
+#print(df.loc[:,["bath","balcony","price"]].head()) #label-based indexing
+
+#iloc is non-inclusive of the last index value, whereas loc is inclusive of the last index value
+#otherwise there's no difference
+df.set_index("balcony")
+print(df.head())
